05aa7883f6896230d63128486510eb43a51ab37b
Natália Satie <naty.odashima@gmail.com>
2022-01-04 17:07:41 -0300

    feat: add change language

--- src/components/ChangeLang/ChangeLang.js
+++ src/components/ChangeLang/ChangeLang.js
@@ -1,22 +1,43 @@
-import { Container } from './MainStyle';
 import { useTranslation } from 'react-i18next';
-import profile from '../images/profile.jpg';
 
-import { Avatar } from '@mui/material';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
 
 const ChangeLang = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation('translation');
+  const [lang, setLang] = useState('pt');
+
+  const handleChange = (event) => {
+    setLang(event.target.value)
+    i18n.changeLanguage(lang);
+  };
+
   return (
-    <Container>
-      asdasdasdasdas
-      <Avatar
-        alt="Natália Satie Odashima profile image"
-        src={profile}
-        sx={{ width: 126, height: 126 }}
-      />
-      <p>{t('name')}</p>
-      <
-    </Container>
+    <div>
+      <Box>
+        <FormControl>
+          <InputLabel id="select-label-change-language">{t('lang.title')}</InputLabel>
+          <Select
+            labelId="select-label-change-language"
+            id="select-label-change-language"
+            value={lang}
+            label={t('lang.title')}
+            onChange={(e) => handleChange(e)}
+          >
+            <MenuItem value='en'>
+              {t('lang.en')}
+            </MenuItem>
+            <MenuItem value='pt'>
+              {t('lang.pt')}
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </div>
   );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,14 +1,17 @@
 import { Container } from './MainStyle';
 import { useTranslation } from 'react-i18next';
 import MyAvatar from '../Avatar/Avatar';
+import ChangeLang from '../ChangeLang/ChangeLang';
 
 const Main = () => {
-  const { t } = useTranslation();
+  const { t } = useTranslation('translation');
+
   return (
     <Container>
       asdasdasdasdasasdasdasdsad
       <MyAvatar />
-      <p>{t('translation:name')}</p>
+      <p>{t('name')}</p>
+      <ChangeLang />
     </Container>
   );
 }
